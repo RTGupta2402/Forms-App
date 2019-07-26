b29d80f7c3ce1914257374a54492c71f465316e5
GUPTA Aarti <aarti.gupta@soprasteria.com>
2019-07-26 15:44:02 +0530

    Reactive Approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,73 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @ViewChild('f', {static: true}) signupForm: NgForm;
+export class AppComponent implements OnInit{
+  // @ViewChild('f', {static: true}) signupForm: NgForm;
 
-  defaultQuestion = 'pet';
-  answer = '';
+  // defaultQuestion = 'pet';
+  // answer = '';
   genders = ['Male', 'Female']  ;
-  user = {
-    username: '',
-    email: '',
-    secret: '',
-    answer: '',
-    gender: ''
-  };
+  // user = {
+  //   username: '',
+  //   email: '',
+  //   secret: '',
+  //   answer: '',
+  //   gender: ''
+  // };
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'Male'
-    // });
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
+  // suggestUserName() {
+  //   const suggestedName = 'Superuser';
+  //   // this.signupForm.setValue({
+  //   //   userData: {
+  //   //     username: suggestedName,
+  //   //     email: ''
+  //   //   },
+  //   //   secret: 'pet',
+  //   //   questionAnswer: '',
+  //   //   gender: 'Male'
+  //   // });
+  //   this.signupForm.form.patchValue({
+  //     userData: {
+  //       username: suggestedName
+  //     }
+  //   });
+  // }
+
+  // // onSubmit(form: NgForm) {
+  // //   console.log(form);
+  // // }
+
+  // onSubmit() {
+  //   // console.log(this.signupForm);
+  //   this.user.username = this.signupForm.value.userData.username;
+  //   this.user.email = this.signupForm.value.userData.email;
+  //   this.user.secret = this.signupForm.value.secret;
+  //   this.user.answer = this.signupForm.value.questionAnswer;
+  //   this.user.gender = this.signupForm.value.gender;
+  // }
+
+  // onReset() {
+  //   this.signupForm.reset();
+  // }
+
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('Male')
     });
   }
 
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
-
   onSubmit() {
-    // console.log(this.signupForm);
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secret = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-  }
-
-  onReset() {
-    this.signupForm.reset();
+    console.log(this.signupForm);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,8 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    //FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
